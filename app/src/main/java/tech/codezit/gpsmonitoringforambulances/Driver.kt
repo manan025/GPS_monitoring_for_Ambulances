5df1dc6d4c0e537a75c74dbe1e835d94f12d1858
manan025 <mananman23@gmail.com>
2020-09-13 18:01:30 +0530

    Final Push for Bon Hackit

--- app/src/main/java/tech/codezit/gpsmonitoringforambulances/Driver.kt
+++ app/src/main/java/tech/codezit/gpsmonitoringforambulances/Driver.kt
@@ -1,9 +1,12 @@
 package tech.codezit.gpsmonitoringforambulances
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_driver.*
+import java.sql.Driver
 
 class Driver : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,12 +15,15 @@ class Driver : AppCompatActivity() {
     }
 
     // When the registration code is entered.
-    fun submit() {
+    fun submit(view: View) {
         if (pin.text!!.length < 6) {
             // create a dialog
             //alternative
             Toast.makeText(applicationContext, "Please enter a valid code", Toast.LENGTH_LONG)
                 .show()
+        } else {
+            val i = Intent(this, DriverDashboard::class.java)
+            startActivity(i)
         }
         val code = pin.text!!.map { it.toInt() }
     }
--- app/src/main/java/tech/codezit/gpsmonitoringforambulances/DriverDashboard.kt
+++ app/src/main/java/tech/codezit/gpsmonitoringforambulances/DriverDashboard.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_driver.*
 
 class DriverDashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +20,7 @@ class DriverDashboard : AppCompatActivity() {
     fun loadSite(view: View) {
         val i = Intent(this, webView::class.java)
         i.putExtra("tracking_url", "https://www.google.com/maps/dir/Patliputra+Medical+College,+Kusum+Vihar,+Koyla+Nagar,+Dhanbad,+Jharkhand/23.8310824,86.5573221/@23.8222863,86.474211,13z/data=!3m1!4b1!4m9!4m8!1m5!1m1!1s0x39f6bcbeeeed355b:0x1be78824b4fa5a24!2m2!1d86.4624047!2d23.8086527!1m0!3e0")
+        startActivity(i)
     }
 
     fun covidDashboard(view: View) {
